cdd186a980066f435b0f3b597b6ab542cca7dac1
Kirill Lakhtachev <lahtachev@gmail.com>
2023-07-21 16:41:46 +0200

    updated styles

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 import App from './components/App.svelte';
 import 'styles/index.scss';
 
-export default new App({ target: document.body });
+export default new App({ target: document.getElementById('app') });
